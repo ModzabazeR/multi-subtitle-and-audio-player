7bd448ae5bc2759e8ae9af3f633e2ffb81f66f00
ModzabazeR <moddaeng.pps@gmail.com>
2022-05-07 16:44:40 +0700

    fix minor bug (2)

--- controls.js
+++ controls.js
@@ -12,7 +12,7 @@ video.onpause = () => {
 const options = {
   video: video, // HTML5 video element
   subUrl: "resources/subtitle/travail_trailer.th.ass", // Link to subtitles
-  fonts: ["resources/fonts/browalia.ttc", "/resources/fonts/zh-cn.ttf"], // Links to fonts (not required, default font already included in build)
+  fonts: ["resources/fonts/browalia.ttc", "resources/fonts/zh-cn.ttf"], // Links to fonts (not required, default font already included in build)
   workerUrl: "subtitles-octopus-worker.js", // Link to WebAssembly-based file "subtitles-octopus-worker.js"
   legacyWorkerUrl: "subtitles-octopus-worker-legacy.js", // Link to legacy JavaScript-based file "subtitles-octopus-worker-legacy.js"
 };
