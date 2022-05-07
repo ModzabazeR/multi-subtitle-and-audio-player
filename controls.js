7d2ce959a79b972a4b8529b19b5faccb3530d518
ModzabazeR <moddaeng.pps@gmail.com>
2022-05-07 16:09:46 +0700

    first commit

---
+++ controls.js
@@ -0,0 +1,36 @@
+const video = document.getElementById("video");
+const audio = document.getElementById("audio");
+
+video.onplay = () => {
+  audio.currentTime = video.currentTime;
+  audio.play();
+};
+video.onpause = () => {
+  audio.pause();
+};
+
+const options = {
+  video: video, // HTML5 video element
+  subUrl: "/resources/subtitle/travail_trailer.th.ass", // Link to subtitles
+  fonts: ["/resources/fonts/browalia.ttc", "/resources/fonts/zh-cn.ttf"], // Links to fonts (not required, default font already included in build)
+  workerUrl: "/subtitles-octopus-worker.js", // Link to WebAssembly-based file "subtitles-octopus-worker.js"
+  legacyWorkerUrl: "/subtitles-octopus-worker-legacy.js", // Link to legacy JavaScript-based file "subtitles-octopus-worker-legacy.js"
+};
+
+const instance = new SubtitlesOctopus(options);
+
+function subtitleHandler() {
+  const lang = document.getElementById("subtitle-select").value;
+  instance.setTrackByUrl(`/resources/subtitle/travail_trailer.${lang}.ass`);
+}
+
+function audioHandler() {
+  video.muted = true;
+  const source = document.getElementById("audioSource");
+  const lang = document.getElementById("audio-select").value;
+
+  source.src = `/resources/audio/travail_trailer.${lang}.m4a`;
+  audio.load();
+  audio.play();
+  audio.currentTime = video.currentTime;
+}
